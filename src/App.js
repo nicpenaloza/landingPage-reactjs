1063eb887b8effc43e512688bc35f688cd22a317
nicpenaloza <nicolasgambact@gmail.com>
2022-02-17 17:40:54 -0500

    navbar setup part1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Navbar from '../src/components/Navbar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar/Container.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
+
+export default function Container() {
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  }
+
+  const closeMobileMenu = () => {
+    setClick(false);
+  }
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo'>
+            <h1>DESIGN</h1>
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+              About
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+              Contact
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/sign-up' className='nav-links' onClick={closeMobileMenu}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        {}
+      </nav>
+    </>
+  )
+}
