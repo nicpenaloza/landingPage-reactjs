afd6b129db52f0d0a98a964e59553af751b76b65
nicpenaloza <nicolasgambact@gmail.com>
2022-02-18 20:44:53 -0500

    main section setup

--- src/components/MainSection/Container.jsx
+++ src/components/MainSection/Container.jsx
@@ -5,12 +5,12 @@ import './style.css';
 export default function Container() {
   return (
     <div className='mainsection'>
+      <video src='/videos/video.mp4' autoPlay loop muted />
       <div className='mainsection-container'>
-        <video src='/videos/video.mp4' autoPlay loop muted/>
         <h1 className='text title'>Welcome</h1>
-        <p className='text'>The best technological solutions on the market</p>
+        <p className='text'>We are EPSILON the best technological solution on the market</p>
         <div className='mainsection-btns'>
-          <Button 
+          <Button
             buttonStyle='btn--outline'
             buttonSize='btn--large'
           >
